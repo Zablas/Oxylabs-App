a1b58f0cdc74821b929c6f3ebd22aef82acd7c08
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-04-17 17:34:34 +0300

    Implement display method in main activity

--- app/src/main/java/com/example/oxylabs_app/MainActivity.kt
+++ app/src/main/java/com/example/oxylabs_app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.oxylabs_app
 
 import android.content.Intent
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,15 +9,32 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val database: NotificationDatabaseHelper = NotificationDatabaseHelper(this)
+    private val notifications: ArrayList<NotificationDTO> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        displayNotificationList()
     }
 
     fun openNewNotificationForm(view: View){
         val intent = Intent(this, NewNotificationFormActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    private fun displayNotificationList(){
+        val cursor = database.getAllNotifications()
+        while (cursor.moveToNext())
+            addNotificationToArray(cursor)
+    }
+
+    private fun addNotificationToArray(cursor: Cursor){
+        val id = cursor.getInt(0)
+        val title = cursor.getString(1)
+        val description = cursor.getString(2)
+        val scheduledTime = cursor.getString(3)
+        notifications.add(NotificationDTO(id, title, description, scheduledTime))
     }
 }
--- app/src/main/java/com/example/oxylabs_app/NotificationDTO.kt
+++ app/src/main/java/com/example/oxylabs_app/NotificationDTO.kt
@@ -1,6 +1,7 @@
 package com.example.oxylabs_app
 
 data class NotificationDTO(
+    val id: Int,
     val title: String,
     val description: String,
     val scheduledTime: String
