2393921f88b5ae44f96ee446dc283ee29b91d630
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-04-17 18:21:53 +0300

    Bind notifications adapter to the recycler view

--- app/src/main/java/com/example/oxylabs_app/MainActivity.kt
+++ app/src/main/java/com/example/oxylabs_app/MainActivity.kt
@@ -5,17 +5,21 @@ import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val database: NotificationDatabaseHelper = NotificationDatabaseHelper(this)
     private val notifications: ArrayList<NotificationDTO> = ArrayList()
+    private val notificationAdapter: NotificationAdapter = NotificationAdapter(this, notifications)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         displayNotificationList()
+        rvNotifications.adapter = notificationAdapter
+        rvNotifications.layoutManager = LinearLayoutManager(this)
     }
 
     fun openNewNotificationForm(view: View){
