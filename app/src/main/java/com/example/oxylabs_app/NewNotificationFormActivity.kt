f28730ca4e280d07d4c31b443d7df97182bcce1a
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-04-18 19:07:51 +0300

    Add a date time picker dialogs to new notification form

--- app/src/main/java/com/example/oxylabs_app/NewNotificationFormActivity.kt
+++ app/src/main/java/com/example/oxylabs_app/NewNotificationFormActivity.kt
@@ -1,19 +1,28 @@
 package com.example.oxylabs_app
 
 import android.app.AlarmManager
+import android.app.DatePickerDialog
 import android.app.PendingIntent
+import android.app.TimePickerDialog
 import android.content.Intent
+import android.icu.util.Calendar
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_new_notification_form.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NewNotificationFormActivity : AppCompatActivity() {
     private val database: NotificationDatabaseHelper = NotificationDatabaseHelper(this)
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_notification_form)
+        txtTime.keyListener = null
     }
 
     fun onCancelClicked(view: View) = onBackPressed()
@@ -56,4 +65,26 @@ class NewNotificationFormActivity : AppCompatActivity() {
     }
 
     private fun wasInsertionSuccessful(insertResult: Long): Boolean = insertResult != -1L
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    fun pickDateTime(view: View) {
+        val currentDateTime = CurrentDateTime(Calendar.getInstance())
+        DatePickerDialog(this, { _, year, month, day ->
+            showTimePickerDialog(currentDateTime, year, month, day)
+        }, currentDateTime.startYear, currentDateTime.startMonth, currentDateTime.startDay).show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun showTimePickerDialog(currentDateTime: CurrentDateTime, year: Int, month: Int, day: Int) {
+        TimePickerDialog(this, { _, hour, minute -> setTimeEditText(year, month, day, hour, minute) },
+            currentDateTime.startHour, currentDateTime.startMinute, true).show()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun setTimeEditText(year: Int, month: Int, day: Int, hour: Int, minute: Int) {
+        val pickedDateTime = Calendar.getInstance()
+        pickedDateTime.set(year, month, day, hour, minute)
+        val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US)
+        txtTime.setText(formatter.format(pickedDateTime.time))
+    }
 }
